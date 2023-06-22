de7b20dc7f6bf873bcad9c738c3bd5c6c0246ef5
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-22 11:14:43 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,9 +4,9 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 const galleryItemsHTML = galleryItems.map(item => `
-<li class="gallery-item">
-  <a class="gallery-link" href="${item.original}">
-    <img class="gallery-image" 
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" 
     src="${item.preview}" 
     data-source="${item.original}" 
     alt="${item.description}" />
