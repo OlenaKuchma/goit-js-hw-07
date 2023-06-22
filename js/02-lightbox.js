03db5bb515055705b23c6a86e130f1617739f0cd
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-22 11:41:38 +0300

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -29,11 +29,37 @@ gallery.addEventListener('click', e => {
 });
 
 
-
-
 console.log(galleryItems);
 
 
+// const gallery = document.querySelector('.gallery');
+
+// // Create the gallery items dynamically
+// galleryItems.forEach(item => {
+//   const galleryItem = document.createElement('a');
+//   galleryItem.href = item.original;
+//   galleryItem.innerHTML = `<img class="gallery__image" src="${item.preview}" alt="${item.description}" />`;
+//   gallery.appendChild(galleryItem);
+// });
+
+// // Initialize SimpleLightbox
+// const lightbox = new SimpleLightbox('.gallery a');
+
+// // Refresh the lightbox after dynamically adding gallery items
+// lightbox.refresh();
+
+// // Attach a click event listener to the <ul> element with class 'gallery'
+// gallery.addEventListener('click', e => {
+//   e.preventDefault();
+
+//   // Check if the clicked element is an <img> element
+//   if (e.target.tagName === 'IMG') {
+//     // Open the lightbox
+//     lightbox.open();
+//   }
+// });
+
+// console.log(galleryItems);
 
 
 
@@ -43,6 +69,3 @@ console.log(galleryItems);
 
 
 
-
-
-console.log(galleryItems);
