adb352359b63d69524b8fe66eeb4cf66c1d52026
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-27 09:36:21 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,24 +3,15 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
+const galleryItemsHTML = galleryItems.map(item => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>
+`).join('');
 
-galleryItems.forEach(item => {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-  
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = item.original;
-  
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = item.preview;
-  image.alt = item.description;
-
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-  gallery.appendChild(galleryItem);
-});
+gallery.innerHTML = galleryItemsHTML;
 
 
 new SimpleLightbox('.gallery a', {
@@ -30,6 +21,33 @@ new SimpleLightbox('.gallery a', {
 });
 
 console.log(galleryItems);
+
+// Я собі це тут залишу для подальшого навчання :)
+// galleryItems.forEach(item => {
+//   const galleryItem = document.createElement('li');
+//   galleryItem.classList.add('gallery__item');
+  
+//   const link = document.createElement('a');
+//   link.classList.add('gallery__link');
+//   link.href = item.original;
+  
+//   const image = document.createElement('img');
+//   image.classList.add('gallery__image');
+//   image.src = item.preview;
+//   image.alt = item.description;
+
+//   link.appendChild(image);
+//   galleryItem.appendChild(link);
+//   gallery.appendChild(galleryItem);
+// });
+
+
+
+
+
+
+
+
 
 
 
